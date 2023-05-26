a77fd55082b0023b21bfa70c04308cff80714535
zhoujie <66541098+zjie1595@users.noreply.github.com>
2023-05-26 16:08:42 +0800

    项目发布到mavenCentral

--- app/src/main/java/com/zj/ffmpeginvoker/MainActivity.kt
+++ app/src/main/java/com/zj/ffmpeginvoker/MainActivity.kt
@@ -2,11 +2,13 @@ package com.zj.ffmpeginvoker
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.github.xch168.ffmpeg.invoker.FFmpegInvoker
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
 }
---
+++ readme.md
@@ -0,0 +1,3 @@
+```groovy
+implementation 'io.github.zjie1595:ffmpeg-invoker:1.0.0'
+```
